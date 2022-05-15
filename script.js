2c06daa60cb78e3a5992cc2e7b5686a8823be23d
Mike Melchior <mike.melchior@outlook.com>
2022-05-15 02:22:45 -0400

    create script to make div grid

--- script.js
+++ script.js
@@ -3,9 +3,30 @@ makeRoot.classList.add('root');
 document.body.appendChild(makeRoot)
 const root = document.querySelector('.root');
 
-for (let i = 1; i <= 16; i++) {
-    const div = document.createElement('div');
-    div.classList.add(`div${i}`);
-    root.appendChild(div);
+
+let gridWidth;
+const checkInputNumber = () => {
+    gridWidth = prompt('Enter a number up to 100 to create the length of an X by X grid.')
+    if (gridWidth > 100) {
+        alert('Number too large');
+        checkInputNumber()
+    }};
+checkInputNumber()
+
+for (let i = 1; i <= gridWidth; i++) {
+    const makeCol = document.createElement('div');
+    makeCol.classList.add(`column${i}`,'column');
+    for (let j = 1; j <= gridWidth; j++) {
+            const makeBox = document.createElement('div');
+            makeBox.classList.add(`row${j}`,'box');
+            const column = document.querySelector('column');
+            makeCol.appendChild(makeBox);
+        }
+    root.appendChild(makeCol);
+   
 }
 
+let columnsLength = document.getElementsByClassName('column');
+
+
+    
